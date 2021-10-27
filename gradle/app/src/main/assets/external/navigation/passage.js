926f31d9ab9dda29ce5c251ba465ea0def6849eb
Teus Benschop <teusjannette@gmail.com>
2021-10-27 11:39:15 +0200

    Android new release
    https://github.com/bibledit/cloud/issues/675

--- gradle/app/src/main/assets/external/navigation/passage.js
+++ gradle/app/src/main/assets/external/navigation/passage.js
@@ -98,11 +98,17 @@ function bindClickHandlers () {
   $("#navigateback").longpress (function (event) {
     historyBack (event);
   });
+  $("#navigateback").on ("contextmenu", function (event) {
+    event.preventDefault();
+  });
   $("#navigateforward").on ("click", function (event) {
     navigateForward (event);
   });
   $("#navigateforward").longpress (function (event) {
     historyForward (event);
+  });
+  $("#navigateforward").on ("contextmenu", function (event) {
+    event.preventDefault();
   });
   $("#selectbook").on ("click", function (event) {
     $ (".fadeout").hide ();
@@ -473,8 +479,6 @@ function historyBack (event) {
 
 function applyHistory (event) {
   event.preventDefault ();
-  console.log("applyHistory");
-  console.log (event);
   if (event.target.localName == "a") {
     $.ajax ({
       url: "/navigation/update",
