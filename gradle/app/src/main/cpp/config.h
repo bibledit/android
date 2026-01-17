aa0f7c7e77db0576b74735d49d9f98083937b10a
Teus Benschop <teusjannette@gmail.com>
2026-01-17 19:59:54 +0100

    C++20 Test code
    https://github.com/bibledit/cloud/issues/1071

--- gradle/app/src/main/cpp/config.h
+++ gradle/app/src/main/cpp/config.h
@@ -4,8 +4,8 @@
 /* Define whether to compile for Android */
 #define HAVE_ANDROID 1
 
-/* define if the compiler supports basic C++17 syntax */
-#define HAVE_CXX17 1
+/* define if the compiler supports basic C++20 syntax */
+#define HAVE_CXX20 1
 
 /* Define whether execinfo.h is present */
 
@@ -87,7 +87,7 @@
 #define PACKAGE_PREFIX_DIR "NONE"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "bibledit 5.1.037"
+#define PACKAGE_STRING "bibledit 5.1.038"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "bibledit"
@@ -96,7 +96,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "5.1.037"
+#define PACKAGE_VERSION "5.1.038"
 
 /* Define to 1 if all of the C89 standard headers exist (not just the ones
    required in a freestanding environment). This macro is provided for
@@ -104,7 +104,7 @@
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "5.1.037"
+#define VERSION "5.1.038"
 
 /* Define whether to compile on Windows */
 /* #undef WIN32 */
