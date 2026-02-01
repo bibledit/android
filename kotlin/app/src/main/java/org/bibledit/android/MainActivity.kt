d4cb2c3d7a27476f7289ce75e5af83a476ade539
Teus Benschop <teusjannette@gmail.com>
2026-02-01 14:51:33 +0100

    Determine the web root location on the device
    https://github.com/bibledit/cloud/issues/1060

--- kotlin/app/src/main/java/org/bibledit/android/MainActivity.kt
+++ kotlin/app/src/main/java/org/bibledit/android/MainActivity.kt
@@ -8,8 +8,10 @@ import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.app.ActivityCompat.requestPermissions
+import java.io.File
 import java.util.Timer
 import kotlin.concurrent.schedule
+
 
 const val REQUEST_CODE = 1000
 
@@ -35,6 +37,27 @@ class MainActivity : AppCompatActivity() {
         // Get the free port number found by the Bibledit library.
         val port: String = GetNetworkPort()
         webAppUrl = "http://localhost:" + port + "/"
+
+        // The directory of the external files.
+        // Usually this is /storage/emulated/0/Android/data/org.bibledit.android/files
+        // Files in this directory cannot be made executable
+        // because system has a protection mechanism for this.
+        val externalDirectory: String = getExternalFilesDir(null)!!.getAbsolutePath()
+
+        // The protected directory.
+        // Usually this is /data/user/0/org.bibledit.android/files normally.
+        // Files there can be set executable.
+        val internalDirectory: String = getFilesDir().getAbsolutePath()
+
+        // Take the external directory for the webroot, if it exists, else the internal directory.
+        var webroot: String = externalDirectory
+        run {
+            val file = File(externalDirectory)
+            if (!file.exists()) webroot = internalDirectory
+        }
+
+
+
 
 
 
