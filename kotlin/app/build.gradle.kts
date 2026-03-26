d022b101b7bd8aee945049e43a4865f09f9b3603
Teus Benschop <teusjannette@gmail.com>
2026-03-26 17:42:40 +0100

    New version
    https://github.com/bibledit/cloud/issues/673

--- kotlin/app/build.gradle.kts
+++ kotlin/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "org.bibledit.android"
         minSdk = 23
         targetSdk = 36
-        versionCode = 175
-        versionName = "5.1.041"
+        versionCode = 176
+        versionName = "5.1.043"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
